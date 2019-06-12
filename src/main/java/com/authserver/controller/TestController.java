6f0d5125db564a37c5099fdfd5f65eb05e5cd080
RobiLee <robi9202@gmail.com>
2019-06-12 23:00:45 +0900

    Initail Commit.

---
+++ src/main/java/com/authserver/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.authserver.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/")
+    public String test(){
+        return "Hello from auth-server!";
+    }
+}
